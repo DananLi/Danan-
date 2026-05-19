005df9c8fde5e88535a15e5b23abd6ca3c38d045
Dananli <dananli@foxmail.com>
2026-05-19 14:26:11 +0800

    Update src/vite-env.d.ts - add web search feature

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -2,6 +2,8 @@
 
 interface ImportMetaEnv {
   readonly VITE_VOLC_API_KEY: string;
+  readonly VITE_GOOGLE_SEARCH_KEY: string;
+  readonly VITE_GOOGLE_SEARCH_CX: string;
 }
 
 interface ImportMeta {
